1e0a56c9ae0a1a28e2005ec65a963695e252b848
A1222h <afrinovatech46@gmail.com>
2026-05-11 22:54:59 +0100

    Correction PWA pour GitHub Pages

--- sw.js
+++ sw.js
@@ -1,14 +1,28 @@
-var CACHE_NAME = 'afrinova-v1';
-var urlsToCache = ['/', '/index.html', '/assets/css/style.css', '/assets/js/main.js'];
+var CACHE_NAME = 'afrinova-v2';
+var urlsToCache = [
+    '/afrinova-site/',
+    '/afrinova-site/index.html',
+    '/afrinova-site/assets/css/style.css',
+    '/afrinova-site/assets/js/main.js',
+    '/afrinova-site/assets/images/logo.png'
+];
 
 self.addEventListener('install', function(event) {
-    event.waitUntil(caches.open(CACHE_NAME).then(function(cache) {
-        return cache.addAll(urlsToCache);
-    }));
+    event.waitUntil(
+        caches.open(CACHE_NAME).then(function(cache) {
+            return cache.addAll(urlsToCache);
+        })
+    );
 });
 
 self.addEventListener('fetch', function(event) {
-    event.respondWith(caches.match(event.request).then(function(response) {
-        return response || fetch(event.request);
-    }));
+    event.respondWith(
+        caches.match(event.request).then(function(response) {
+            return response || fetch(event.request).catch(function() {
+                if (event.request.mode === 'navigate') {
+                    return caches.match('/afrinova-site/index.html');
+                }
+            });
+        })
+    );
 });
